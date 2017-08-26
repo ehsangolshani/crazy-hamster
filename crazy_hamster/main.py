596d7d4026cb011d77a13cbae426fcb644646e76
Ehsan <ehsanroman74@gmail.com>
2017-08-26 11:34:00 +0430

    remove main

--- crazy_hamster/main.py
+++
@@ -1,5 +0,0 @@
-from crazy_hamster.my_class import MyClass
-
-if __name__ == "__main__":
-    obj = MyClass("Hello", "world")
-    obj.print()
--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 setuptools.setup(name='crazy hamster',
                  version='0.1',
                  description='The craziest hamster in the world',
-                 url='http://github.com/storborg/funniest',
+                 url='https://github.com/ehsangolshani/crazy-hamster',
                  author='Ehsan Golshani',
                  author_email='ehsanroman74@gmail.com',
                  license='GNU',
-                 packages=['crazy_hamster', 'aiohttp', 'asyncio'],
+                 packages=['crazy_hamster'],
                  zip_safe=False)

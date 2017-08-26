f59b9ae0fd6acbb7f152a11bba315b8e145bd80a
Ehsan <ehsanroman74@gmail.com>
2017-08-26 11:15:56 +0430

    initial commit

---
+++ crazy_hamster/main.py
@@ -0,0 +1,5 @@
+from crazy_hamster.my_class import MyClass
+
+if __name__ == "__main__":
+    obj = MyClass("Hello", "world")
+    obj.print()
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='crazy hamster',
+                 version='0.1',
+                 description='The craziest hamster in the world',
+                 url='http://github.com/storborg/funniest',
+                 author='Ehsan Golshani',
+                 author_email='ehsanroman74@gmail.com',
+                 license='GNU',
+                 packages=['crazy_hamster', 'aiohttp', 'asyncio'],
+                 zip_safe=False)
